0c209536a9f4b32e44bf57667a834f187ab5e8db
SHIVAM SINGH <91686685+Shivams9@users.noreply.github.com>
2022-11-16 20:17:43 +0530

    Update reverse methods.py

--- reverse methods.py
+++ reverse methods.py
@@ -1,3 +1,19 @@
+#left < right   (left hmesa right se chota hota hai coding me ---   on page once,tens,etc right se suru hota hai coding me left se right hota hai)
+#self created fuction of position for replace position
+def reverse(p,left,right):
+    while left < right:
+        a[left], a[right] = a[right], a[left]
+        left += 1
+        right -= 1
+
+a = [1, 2, 3, 4, 5, 6, 7]
+print(a)
+reverse(a,0,2)      #(a = array(input) 0=position 2=end position jo replace karega)
+print(a)
+
+
+
+
 #------method
 a = [1,2,3,4,5]
 a.reverse()
